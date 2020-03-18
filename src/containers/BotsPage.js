12d4fd448ab49774e4fd5f5c4b4e166d3618d478
github username <github email address>
2020-03-18 11:57:54 -0500

    Olivia's practice challenge

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection.js';
+import YourBotArmy from './YourBotArmy.js';
 
 class BotsPage extends Component {
   //start here with your code for step one
+  // constructor(){
+  //   super();
+  // }
 
+  
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+      <YourBotArmy army={this.props.army} toggleSelected={this.props.toggleSelected} />
+      <BotCollection bots={this.props.bots} toggleSelected={this.props.toggleSelected}/>
+    </div>;
   }
 }
 
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BotCard from '../components/BotCard.js'
 
 class YourBotArmy extends Component {
   //your bot army code here...
@@ -7,8 +8,10 @@ class YourBotArmy extends Component {
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
-          <div className="row bot-army-row">
-            {/*...and here...*/}
+          <div className="row bot-army-row" >
+            {this.props.army.map(bot => {
+              return <BotCard bot={bot} onClick={() => this.props.deSelectBot(bot.id)} />
+            })}
             Your Bot Army
           </div>
         </div>
