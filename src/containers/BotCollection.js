cf3789682f0b64fc295b1ef78f21eaa3ab152602
Linda <ltramos7@gmail.com>
2020-03-18 11:58:41 -0500

    i tried

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import BotCard from '../components/BotCard'
 
 class BotCollection extends Component {
   //your code here
 
+  //map through the robots array to put them into the BotCard component
+
+  mapRobots(){
+    
+    return this.props.robots.map(robot => {
+     return <BotCard robotClick={this.props.robotClick} bot={robot}/>
+  })}
+
   render() {
+    // console.log(this.props.robotClick)
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
+          {this.mapRobots()}
+          
           Collection of all bots
         </div>
       </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,62 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+import BotCard from '../components/BotCard'
 
 class BotsPage extends Component {
   //start here with your code for step one
+  constructor(){
+    super()
+    this.state = {
+      robots: [],
+      included: false
+    }
+  }
+  handleRobotClick(){
+    this.setState({
+      included: !this.state.included
+    })
+   }
+
+    // handleClick() {
+  //   this.setState(state => ({
+  //     isToggleOn: !state.isToggleOn
+  //   }));
+  // }
+
+  componentDidMount(){
+    fetch("http://localhost:6001/bots/")
+    .then( resp => resp.json() )
+    .then( robotData => this.setState({
+      robots: robotData
+    }) )
+  }
+ 
+  
+
+
+  //if the botCard is clicked want to add that botCard to YoourBotArmy component
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+    <BotCollection robotClick={this.handleRobotClick} robots={this.state.robots}/>
+    <YourBotArmy showRobot={this.handleRobotClick} something={this.state.included ? <BotCard /> : null}/>
+    </div>;
   }
 }
+
+{/* render() {
+  return (
+    <button onClick={this.handleClick}>
+      {this.state.isToggleOn ? 'ON' : 'OFF'}
+    </button>
+  );
+} */}
+
+
+
+//YourBotArmy will have robot cards appear when a robot is clicked on. 
+//need to add event listener to the cards
+//onCLick event listener will render a card to the YorBotArmy container
 
 export default BotsPage;
