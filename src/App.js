2a35384994452752c18a6aab35016bea8880fa75
Niko Dixon <nikodixon@gmail.com>
2020-03-18 11:59:59 -0500

    I have a lotttt of work to do.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,54 @@ import BotsPage from "./containers/BotsPage";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      bots:[]
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('http://localhost:6001/bots')
+    .then(resp => resp.json())
+    .then(botsData =>  {
+      const updatedBots = 
+      botsData.map( botObj => {
+        return {
+          ...botObj, 
+          inArmy: false
+        }
+
+    })
+      this.setState({
+        bots: updatedBots
+      })
+    })
+
+  }
+
+  addBotToArmy = (clickedBot) => {
+    const updatedBots = this.state.bots.map(bot => {
+      if(clickedBot.id === bot.id) {
+        return {
+          ...bot,
+          inArmy: true
+        }
+      } else {
+        return bot 
+      }
+    })
+    this.setState({
+      bots: updatedBots
+    })
+
+  }
+  
   render() {
     return (
       <div className="App">
-        <BotsPage />
+        <BotsPage addBotToArmy={this.addBotToArmy} bots={this.state.bots} />
       </div>
     );
   }
--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import BotCard from "../components/BotCard"
+
 
 class BotCollection extends Component {
   //your code here
-
+  
+  
   render() {
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
+        {this.props.bots.map(bot => {
+       return <BotCard addBotToArmy={this.props.addBotToArmy}   bot={bot} />
+     })}
           Collection of all bots
         </div>
       </div>
