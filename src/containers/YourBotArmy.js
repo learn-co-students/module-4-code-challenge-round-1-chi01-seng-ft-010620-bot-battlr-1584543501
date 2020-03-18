152a59d1c80f89e2656bd0e8334120a26be99d97
John Leavell <johnleavell@gmail.com>
2020-03-18 11:58:17 -0500

    done

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 
 class YourBotArmy extends Component {
-  //your bot army code here...
 
+  
+  
   render() {
+    console.log(this.props)
+    const { bot } = this.props
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+            <p>Your Bot Army</p>
+            <p>Name: {bot.name}</p>
+            <p>Healt: {bot.health}</p>
+            <img src={bot.avatar_url} />
           </div>
         </div>
       </div>
