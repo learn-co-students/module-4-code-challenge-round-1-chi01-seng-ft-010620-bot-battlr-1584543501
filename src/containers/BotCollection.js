df47459d140afeda204e39f2ae9e9284936d1cf9
learn <github email address>
2020-03-18 09:58:38 -0700

    slow System

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-
+import BotCard from '../components/BotCard'
 class BotCollection extends Component {
   //your code here
 
   render() {
+    
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
+          {this.props.botArr.map(botObj=> {
+            return <BotCard key={botObj.id} addBot={this.props.addBot} bot={botObj}/>
+          })}
           Collection of all bots
         </div>
       </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
-
+import BotCollection from '../containers/BotCollection'
+import YourBotArmy from '../containers/YourBotArmy'
 class BotsPage extends Component {
   //start here with your code for step one
+  constructor(){
+    super()
+    this.state = {
+      bots: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/bots')
+    .then(resp => resp.json())
+    .then(botsData => {const newBots = botsData.map(botsObj => {
+      return {
+        ...botsObj, inArmy: false
+      }
+    })
+  this.setState({
+    bots: newBots
+  })
+  }
+  )
+  }
+
+
+  
+  addBot = (e) => {
+    console.log(e)
+    // switches inArmy to false for bot with id of e. 
+  }
+
+  dischardBot = (botId) => {
+    fetch(`http://localhost:6001/bots/${botId}`,deleteObj)
+    .then(resp => resp.json())
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    
+    return <div>
+      <BotCollection addBot={this.addBot} botArr={this.state.bots}/>
+      <YourBotArmy />
+    </div>;
   }
 }
 
